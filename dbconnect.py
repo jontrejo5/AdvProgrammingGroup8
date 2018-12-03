3fb91b9648e6cdd3d516e665bf8e66fc09f49fd8
jontrejo5 <jtrejo0515@gmail.com>
2018-12-03 15:04:29 -0700

    made home.html a login page; makes a get request but the request leads to the error page

--- dbconnect.py
+++ dbconnect.py
@@ -2,13 +2,13 @@ import pymysql
 
 db = pymysql.connect("localhost", "root", "", "advprog")
 
+def insertuser(username, password):
 
-
-try:
-    with db.cursor() as cursor:
-        sql = "INSERT INTO `users` (`username`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        print(result)
-finally:
-    db.close()
+    try:
+        with db.cursor() as cursor:
+            sql = "INSERT INTO `users` (`username`, `password`) VALUES (%s, %s)"
+            cursor.execute(sql)
+            result = cursor.fetchone(username, password)
+            print(result)
+    finally:
+        db.close()
